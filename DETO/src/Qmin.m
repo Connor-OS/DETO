9572aa73dc7fa06c233633cee537b4d907b92a14
Enrico <enrico.masoero2@gmail.com>
2021-05-01 16:30:19 +0200

    Added 4 nonlinear potentials and option to dump iteration 1 trajectory
    - added input option to select among 5 potential types, 4 of which are nonlinear ("e-x" is softening in tension and harding in compression,  "e+x” is hardening in tension and softening in compression, "tanh(x)" is softening both ways, and "sinh(x)" is hardening both ways)
    - added inputs potential parameter “apot” (on top of previous “kspr”). Potential type “lin_spr” does not use apot
    - added input option to provide a damage matrix: see line 29 etc. for a description of its structure. Currently the matrix is not usable, so one can provide anything and the simulation will be unaffected
    - Edited Qmin.m and top.m to manage the various types of potentials and related sensitivities and objective functions
    - added the hard-coded option “dump1 = bool” in line 14 of top.m, to turn on/off the dumping of the deformation trajectory during Qmin of iteration 1 for visualization in OVITO. This now-called "it1.dump" file  can help understand the behaviour of nonlinear systems during the first iteration, when the system is least stiff - in particular, see if a structure collapsed during step one causing the optimization not to converge

--- DETO/src/Qmin.m
+++ DETO/src/Qmin.m
@@ -4,13 +4,20 @@ tol_min = 5e-10;
 tol = 2*tol_min;
 nstep = 0;
 dmax = 0.01;
+%dmax = 0.2;
 dt = 0.01;
+%dt = 200.;
 Eer = 0;
 low_m = min(m);
+%Fmax = 0;
+
+if (dump1==true && loop==1) df1 = fopen('../dump/it1.dump','w'); end
+
 % Euler integration
 while tol > tol_min
     nstep = nstep + 1;
     Fx = Fxe;  Fy = Fye;
+    Fmax = 0;
     % Compute force
     for i=1:length(x)
         for s=1:nn(i)
@@ -18,7 +25,14 @@ while tol > tol_min
             dx = x(j)-x(i);     dy = y(j)-y(i);
             L(i,s) = sqrt( dx^2 + dy^2 );
             if j > i
-                F = k(i,s) * (L(i,s) - Li(i,s));
+                if (ptype == 1) F = k(i,s) * (L(i,s) - Li(i,s));
+                elseif (ptype == 2) F = k(i,s)/apot * (-exp( -apot*(L(i,s) - Li(i,s)))+1 );
+                elseif (ptype == 3) F = k(i,s)/apot * (exp( apot*(L(i,s) - Li(i,s)))-1 );
+                elseif (ptype == 4) F = k(i,s)/apot * (tanh( apot*(L(i,s) - Li(i,s))) );
+                elseif (ptype == 5) F = k(i,s)/apot * (sinh( apot*(L(i,s) - Li(i,s))) );
+                end
+                    
+                %if (abs(F)>Fmax) Fmax=abs(F); end
                 Fxx = F * dx/L(i,s);    Fyy = F * dy/L(i,s);
                 Fx(i) = Fx(i) + Fxx;    Fx(j) = Fx(j) - Fxx;
                 Fy(i) = Fy(i) + Fyy;    Fy(j) = Fy(j) - Fyy;
@@ -65,12 +79,34 @@ while tol > tol_min
     for i = 1:length(xi)
         for s = 1:nn(i)
             j = N(i,s);
-            Eer = Eer+1/4*m(i)^2*m(j)^2*kspr*(L(i,s)-Li(i,s))^2;
+            % all potential factors divided by two due to neighbour double counting
+            rij = L(i,s)-Li(i,s);
+            if (ptype == 1) potfac = 1./4. * kspr*(rij^2);
+                elseif (ptype == 2) potfac = 1./2. * ( kspr/apot * (exp(-apot*rij)/apot+rij ) - kspr/apot/apot );
+                elseif (ptype == 3) potfac = 1./2. * ( kspr/apot * (exp(apot*rij)/apot-rij ) - kspr/apot/apot );
+                elseif (ptype == 4) potfac = 1./2. * kspr/apot/apot * log(cosh(apot*rij));
+                elseif (ptype == 5) potfac = 1./2. * ( kspr/apot/apot * cosh(apot*rij) - kspr/apot/apot );
+            end
+            Eer = Eer + potfac * m(i)^2 *m(j)^2;   
+          
+            %Eer = Eer+1/4*m(i)^2*m(j)^2*kspr*(L(i,s)-Li(i,s))^2;
         end
     end
     if nstep - freeze_step < 5
         tol = 2*tol_min;
     else
-        tol = abs(Eer-Eold)/Eold;
+        tol = abs(Eer-Eold)/abs(Eold);
     end
+    
+    
+    % dumping xyz evolution during first iteration, for debugging 
+    if (dump1==true && loop==1 && mod(nstep,1000)==1)
+        fprintf(df1,[sprintf('%4i\n',length(m)),'Frame.: ',sprintf('%4i\n',nstep)]);
+        for i=1:length(x)
+        fprintf(df1,[sprintf('%4i ',i) sprintf('%6.3f ',x(i)) sprintf('%6.3f ',y(i)) ...
+            sprintf('%6.3f \n',m(i)) ]);
+         end
+    end
+    
 end
+if (dump1==true && loop==1) fclose(df1); end
--- DETO/src/top.m
+++ DETO/src/top.m
@@ -1,7 +1,46 @@
 % A DISCRETE ELEMENT TOPOLOGY OPTIMIZATION CODE BY CONNOR O'SHAUGHNESSY 2020%
-function top(nelx,nely,mass,Diam,rmin,kspr)
+function top(nelx,nely,mass,Diam,rmin,pot,kspr,apot,Mdam)
+% nelx, nely = number of particles in x and y directions
+% mass = total target mass of the system (optimisation constraint)
+% Diam = particle diameter
+% rmin = filtering radius
+% pot = potential type, can be 'lin_spr', 'e-x', $e+x', tanh(x), sinh(x)
+% kspr, apot = potential parameters (for linear type, only kspr is used)
+% Mdam = damage matrix with damage x,y positions, radius, and weight factor
+
+disp('DETO STARTED');
 tic
-% INITIALIZEATION
+
+dump1 = false;   % if dump=true, the deformation history during iteration 1 is dumped
+
+ptype = 0;  % potential type, lin_spr=1, e-x=2, e+x=3, tanh(x)=4, sinh(x)=5
+if (strcmp(pot,'lin_spr')==1)   ptype = 1; end
+if (strcmp(pot,'e-x')==1)   ptype = 2; end
+if (strcmp(pot,'e+x')==1)   ptype = 3; end
+if (strcmp(pot,'tanh(x)')==1)   ptype = 4; end
+if (strcmp(pot,'sinh(x)')==1)   ptype = 5; end
+if (ptype == 0)   
+    msg = sprintf('\nERROR: unknown type of interaction: %s',pot);
+    disp(msg);
+    return
+end
+
+% Printing input damage matrix, just to check it is alright
+if size(Mdam,1)==0
+    disp('\nEmpty damage matrix provided. Optimising structure without damage.')
+else
+    msg = sprintf('\nNumber of damage scenarios: %d',size(Mdam,1));
+    disp(msg);
+    disp('X-pos, Y-pos, radius and weight of damage:');
+    disp('Xdam  Ydam  Rdam  Wdam');
+    for i=1:size(Mdam,1)
+        msg = sprintf('%f %f %f %f',Mdam(i,1),Mdam(i,2),Mdam(i,3),Mdam(i,4));
+        disp(msg);
+    end
+end
+
+
+% INITIALIZATION
 m = mass*ones((nely*nelx)-floor(nely/2),1);
 cut = 1.01*Diam;
 xi = zeros(length(m),1);  yi = xi;
@@ -27,29 +66,70 @@ Itt = fopen(it,'w');
 % START ITERATION
 while change > 0.004
     loop = loop+1;
+    msg = sprintf('\nITERATION %d',loop);
+    disp(msg);
     mold = m;
+    % updtaing interaction stiffnesses based on latest m-variables
     for i = 1:length(m)
         for s = 1:nn(i)
             j = N(i,s);
             k(i,s) = m(i)^2 *m(j)^2  * kspr;
         end
     end
+    
+    % ------------------   damage loop starts here here 
+    % before entering loop must define a combined sensitivity vector to sum
+    % individual damage weighted sensitivities. 
+    
+    % Each time a new damage is assessed we must resume the initial confir   x = xi; y = yi;
+   
+    % For each damage, make list of removed elements ids, record their masses, then set their m =
+    % 0, run Qmin, in filtering hard kill by forcing dc/dxi = 0 for removed masses
+    
+    % Before moving to next damage (or out into optimisation step), restore
+    % original masses and add weighted contribution to global sensitivity
+    % vector
+    
+    % After damage loop, record the combined sensitivity vector into dc:
+    % this is the dc to which the volume constraint will be applied in
+    % OC
+    
     % DEM-ANALYSIS assuming harmonic potential
+    disp('RUNNING DEM MINIMISATION...');
     Qmin
+    %msg = sprintf('\nMax Force on spring is %e',Fmax);
+    %disp(msg);
+    disp('... DONE');
+    
     % Cost function and sensitivities
-    Eer = 0;
+    %Eer = 0;     % Eer already comes from Qmin - no need to recompute it
     dc = zeros(length(xi),1);
     for i = 1:length(xi)
         for s = 1:nn(i)
             j = N(i,s);
-            Eer = Eer+1/4*m(i)^2*m(j)^2*kspr*(L(i,s)-Li(i,s))^2; % 1/4 cause double counting each interaction
-            dc(i) = dc(i)-0.5*m(i)*m(j)^2*kspr*(L(i,s)-Li(i,s))^2;
+            %Eer = Eer+1/4*m(i)^2*m(j)^2*kspr*(L(i,s)-Li(i,s))^2; % 1/4 cause double counting each interaction
+            rij = L(i,s)-Li(i,s);
+            if (ptype == 1) potfac = 1./4. * kspr*(rij^2);
+                elseif (ptype == 2) potfac = 1./2. * ( kspr/apot * (exp(-apot*rij)/apot+rij ) - kspr/apot/apot );
+                elseif (ptype == 3) potfac = 1./2. * ( kspr/apot * (exp(apot*rij)/apot-rij ) - kspr/apot/apot );
+                elseif (ptype == 4) potfac = 1./2. * kspr/apot/apot * log(cosh(apot*rij));
+                elseif (ptype == 5) potfac = 1./2. * ( kspr/apot/apot * cosh(apot*rij) - kspr/apot/apot );
+            end
+            %dc(i) = dc(i)-0.5*m(i)*m(j)^2*kspr*(L(i,s)-Li(i,s))^2;
+            dc(i) = dc(i) - 2.*m(i)*m(j)^2 * potfac;
         end
+       % msg = sprintf('dc(%d) = %e',i,dc(i));
+        %disp(msg);
     end
+
     % Filtering of sensitivities (aka coarse graining)
     if rmin > Diam
         Mesh_filter
     end
+    
+    % ------------------   damage loop finishes here
+    
+    
     % DESIGN UPDATE BY THE OPTIMALITY CRITERIA METHOD
     OC
     % PER PARTICLE STRESS
@@ -78,6 +158,7 @@ while change > 0.004
 fprintf(Itt,[' Sim time.: ' sprintf( '%6.3f\n', toc)]);
 %%%%% POST PROCESSING %%%%%%%
 post_processing
+disp('\nDETO DONE: SUCCESS');
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This Matlab code was written by Enrico Masoero and Connor O'Shaughnessy  %

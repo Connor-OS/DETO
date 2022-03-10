4503c47d64b3dc3f11ccf65ba2162a166d1c07dd
Enrico <enrico.masoero2@gmail.com>
2022-03-10 16:05:47 +0000

    recorded vectors of inputs and attributes in simulations class

--- DETO_3D/src/simulations.cpp
+++ DETO_3D/src/simulations.cpp
@@ -41,16 +41,61 @@ Simulations::~Simulations()
 void Simulations::add(std::string read_string)
 {
 // record name in a vector
-    std::string sim_name, sim_type, repeatfname;
-    bool is_repeat;
+    std::string sim_name, sim_type, repstr, repYN;
+    
     std::istringstream lss(read_string);
-    lss >> sim_name >> sim_type;
+    lss >> sim_name >> sim_type >> repstr >> repYN;
     sim_names.push_back(sim_name);
     sim_types.push_back(sim_type);
+    if (strcmp(repstr.c_str(), "repeat") != 0) {
+        std::string msg = "Error: keyword \"repeat\" must follow simulation type in input script\n";
+        error->errsimple(msg);
+    }
+    if (strcmp(repYN.c_str(), "yes") == 0){
+        sim_is_repeat.push_back(1);
+        std::string repfname;
+        lss >> repfname;
+        sim_repeat_file.push_back(repfname);
+    }
+    else if (strcmp(repYN.c_str(), "no") == 0) {
+        sim_is_repeat.push_back(0);
+        sim_repeat_file.push_back("NULL");
+    }
+    else {
+        std::string msg = "Error: repeat must be \"yes\" or \"no\", case sensitive. Instead I found "+repYN+"\n";
+        error->errsimple(msg);
+    }
+    
+    
+    // default values for additional inputs related to the simulations - will be overwritten if sim is "cstgs"
+    cstgs_varname.push_back("NULL");
+    cstgs_type.push_back("NULL");
+    cstgs_par1.push_back(-1.);
+    cstgs_par2.push_back(-1.);
+    cstgs_tol.push_back(100000000);
+    
+    if (strcmp(sim_type.c_str(), "cstgs") == 0) {
+        int pos = cstgs_varname.size()-1;
+        std::string varname, tolstr, ttype;
+        double par1, par2, tol;
+        lss >> varname >> ttype >> par1 >> par2;
+        cstgs_varname[pos] = varname;
+        cstgs_type[pos] = ttype;
+        cstgs_par1[pos] = par1;
+        cstgs_par2[pos] = par2;
+        if (strcmp(ttype.c_str(), "binary_chop") == 0) {
+            lss >> tolstr >> tol;
+            if (strcmp(tolstr.c_str(), "tol") != 0) {
+                std::string msg = "Error: tolerance must be specified through \"tol\" keyword when using binary_chop";
+                error->errsimple(msg);
+            }
+            cstgs_tol[pos] = tol;
+        }
+    }
+    
+    cstgs_crit.push_back("NULL");   // this will be provided separaetly by the user through dedicated "Criterion sim_ID string" command in the input scrupt
+    
     sim_attributes.push_back(std::vector<std::string>());
-    // record type in a vector and, depending on type, record associated parameters (none if "run", some if "cstgs"
-    // record repeat yes/no in a boolean vector
-    // record repeat attributes in a vector (empty entry if repeat = no, otherwsie whatever is needed for the repeat (I think a file to load something from)
     
 }
 
@@ -65,8 +110,12 @@ void Simulations::add_attribute(std::string read_string)
     lss >> sim_ID;
     for(int i = 0; i < sim_names.size(); i++){
         if(strcmp(sim_ID.c_str(),sim_names[i].c_str()) == 0){
-            sim_attributes[i].push_back(lss.str());
-            fprintf(screen,"simulations -- in %s adding attribute %s \n",sim_ID.c_str(),lss.str().c_str());
+            std::string read_string2;
+            std::getline(lss, read_string2);
+            sim_attributes[i].push_back(read_string2);
+            if (me==MASTER){
+            fprintf(screen,"simulations -- in %s adding attribute %s \n",sim_names[i].c_str(),sim_attributes[i][sim_attributes[i].size()-1].c_str());
+            }
         }
     }
 }
--- DETO_3D/src/simulations.h
+++ DETO_3D/src/simulations.h
@@ -24,10 +24,16 @@ namespace DETO_NS {
     
         int me;     // id of the current processor (rank)
         std::vector<std::string> sim_names; //vector holding sim names
-        std::vector<std::string> sim_types; //vector holding sim types
+        
+        std::vector<std::string> sim_types; //vector holding sim types (run or cstgs)
+        std::vector<std::string> cstgs_varname;    // name of variables to be changed during a cstgs simulation
+        std::vector<std::string> cstgs_type;    // type of change (binary_chop or increment) for cstgs sims
+        std::vector<double> cstgs_par1,cstgs_par2;    // parameters guiding update of var in cstgs sims
+        std::vector<double> cstgs_tol;    // tolerance for cgtgs of binary_chop type
+        std::vector<std::string> cstgs_crit; //criterion to escape a cstgs sim
+        
         std::vector<bool> sim_is_repeat; //vector of bool specifying if repeat
         std::vector<std::string> sim_repeat_file; //vector holding path to repeat file data
-
         std::vector<std::vector<std::string>> sim_attributes; //vector sim attributes
 
         void printall();
